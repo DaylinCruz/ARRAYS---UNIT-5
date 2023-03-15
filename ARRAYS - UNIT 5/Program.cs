00530335d4289b652e977b3d7f87fb7204caf61f
dayli <dayli@DESKTOP-VP1QNAQ>
2023-03-14 22:34:55 -0600

    Agregar archivos de proyecto.

---
+++ ARRAYS - UNIT 5/Program.cs
@@ -0,0 +1,82 @@
+﻿
+string[] color = new string[6];
+
+color[0] = "blue";
+color[1] = "purple";
+color[2] = "pink";
+color[3] = "yellow";
+color[4] = "white";
+color[5] = "orange";
+
+Console.WriteLine(color[1]);
+Console.WriteLine(color[5]);
+
+////////////////////////////////////
+
+string[] planetas = new string[8];
+
+planetas[0] = "mercurio";
+planetas[1] = "venus";
+planetas[2] = "tierra";
+planetas[3] = "marte";
+planetas[4] = "jupiter";
+planetas[5] = "saturno";
+planetas[6] = "urano";
+planetas[7] = "néptuno";
+
+Console.WriteLine(planetas[5]);
+
+////////////////////////////////////
+
+string[] ciudades = new string[5];
+
+ciudades[0] = "ITALIA";
+ciudades[1] = "HOLANDA";
+ciudades[2] = "TAILANDIA";
+ciudades[3] = "FINLANDIA";
+ciudades[4] = "BRASIL";
+
+Console.WriteLine(ciudades[0]);
+Console.WriteLine(ciudades[2]);
+
+////////////////////////////////////
+
+int[] hijos = new int[5];
+
+hijos[0] = 2;
+hijos[1] = 4;
+hijos[2] = 1;
+hijos[3] = 4;
+hijos[4] = 3;
+
+Console.WriteLine(hijos[3]);
+Console.WriteLine(hijos[4]);
+Console.WriteLine(hijos[0]);
+
+////////////////////////////////////
+
+int[] calzado = new int[4];
+
+calzado[0] = 20;
+calzado[1] = 79;
+calzado[2] = 165;
+calzado[3] = 84;
+
+Console.WriteLine(calzado[2]);
+
+////////////////////////////////////
+
+string[] helado = new string [7];
+
+helado[0]= "VAINILLA";
+helado[1]= "CHOCOLATE";
+helado[2]= "NAPOLITANO";
+helado[3]= "GRANADA";
+helado[4]= "PITAYA";
+helado[5]= "ALGODON DE AZUCAR";
+helado[6]= "MENTA CON CHOCOLATE";
+
+Console.WriteLine(helado[2]);
+Console.WriteLine(helado[6]);
+Console.WriteLine(helado[0]);
+
